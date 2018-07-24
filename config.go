516e30eee6d9b575f46b745b01f32ef3a555b0ce
z0mbie42 <sylvain.kerkour@gmail.com>
2018-07-24 12:41:28 +0200

    config: use astro.LoggerOption type

--- config.go
+++ config.go
@@ -5,70 +5,70 @@ import (
 	"time"
 )
 
-func SetWriter(writer io.Writer) func(*Logger) error {
+func SetWriter(writer io.Writer) LoggerOption {
 	return func(logger *Logger) error {
 		logger.writer = writer
 		return nil
 	}
 }
 
-func SetFormatter(formatter Formatter) func(*Logger) error {
+func SetFormatter(formatter Formatter) LoggerOption {
 	return func(logger *Logger) error {
 		logger.formatter = formatter
 		return nil
 	}
 }
 
-func SetWith(fields ...interface{}) func(*Logger) error {
+func SetWith(fields ...interface{}) LoggerOption {
 	return func(logger *Logger) error {
 		logger.fields = fields
 		return nil
 	}
 }
 
-func SetInsertTimestampField(insert bool) func(*Logger) error {
+func SetInsertTimestampField(insert bool) LoggerOption {
 	return func(logger *Logger) error {
 		logger.insertTimestampField = insert
 		return nil
 	}
 }
 
-func SetLevel(level Level) func(*Logger) error {
+func SetLevel(level Level) LoggerOption {
 	return func(logger *Logger) error {
 		logger.level = level
 		return nil
 	}
 }
 
-func SetTimestampFieldName(fieldName string) func(*Logger) error {
+func SetTimestampFieldName(fieldName string) LoggerOption {
 	return func(logger *Logger) error {
 		logger.timestampFieldName = fieldName
 		return nil
 	}
 }
 
-func SetMessageFieldName(fieldName string) func(*Logger) error {
+func SetMessageFieldName(fieldName string) LoggerOption {
 	return func(logger *Logger) error {
 		logger.messageFieldName = fieldName
 		return nil
 	}
 }
 
-func SetLevelFieldName(fieldName string) func(*Logger) error {
+func SetLevelFieldName(fieldName string) LoggerOption {
 	return func(logger *Logger) error {
 		logger.levelFieldName = fieldName
 		return nil
 	}
 }
 
-func SetTimestampFunc(fn func() time.Time) func(*Logger) error {
+func SetTimestampFunc(fn func() time.Time) LoggerOption {
 	return func(logger *Logger) error {
 		logger.timestampFunc = fn
 		return nil
 	}
 }
 
-func AddHook(hook Hook) func(*Logger) error {
+func AddHook(hook Hook) LoggerOption {
 	return func(logger *Logger) error {
 		logger.hooks = append(logger.hooks, hook)
 		return nil
