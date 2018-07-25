3db49a2683dccbce85c51c8d3403bea203ca119f
z0mbie42 <sylvain.kerkour@gmail.com>
2018-07-25 09:08:50 +0200

    logger: add the cli_formatter

--- _examples/main.go
+++ _examples/main.go
@@ -30,10 +30,14 @@ func main() {
 	}
 
 	subLogger := log.With("contextual_field", 42)
+	subLogger.Config(
+		astroflow.SetFormatter(astroflow.NewCLIFormatter()),
+	)
 
 	log.Info("info from logger")
 	log.With("field1", "hello world", "field2", 999.99).Info("info from logger with fields")
 	subLogger.Debug("debug from sublogger")
+	subLogger.Info("info form subLogger")
 	subLogger.Warn("warning from sublogger")
 	subLogger.Error("error from sublogger")
 }
---
+++ cli_formatter.go
@@ -0,0 +1,92 @@
+package astroflow
+
+import (
+	"bytes"
+	"fmt"
+	"sort"
+	"strconv"
+	"time"
+
+	"github.com/json-iterator/go"
+)
+
+type CLIFormatter struct {
+	NoColor            bool
+	TimestampFieldName string
+	LevelFieldName     string
+	MessageFieldName   string
+}
+
+func NewCLIFormatter() CLIFormatter {
+	return CLIFormatter{
+		TimestampFieldName: TimestampFieldName,
+		MessageFieldName:   MessageFieldName,
+		LevelFieldName:     LevelFieldName,
+		NoColor:            false,
+	}
+}
+
+func (formatter CLIFormatter) Format(event Event) []byte {
+	var ret = new(bytes.Buffer)
+
+	lvlColor := cReset
+	level := ""
+	if l, ok := event[formatter.LevelFieldName].(string); ok {
+		if !formatter.NoColor {
+			lvlColor = levelColor(l)
+		}
+		level = l
+	}
+
+	message := ""
+	if m, ok := event[formatter.MessageFieldName].(string); ok {
+		message = m
+	}
+
+	if level != "" {
+		ret.WriteString(colorize("• ", lvlColor, !formatter.NoColor))
+	}
+	ret.WriteString(message)
+
+	fields := make([]string, 0, len(event))
+	for field := range event {
+		switch field {
+		case formatter.TimestampFieldName, formatter.MessageFieldName, formatter.LevelFieldName:
+			continue
+		}
+
+		fields = append(fields, field)
+	}
+
+	sort.Strings(fields)
+	for _, field := range fields {
+		if needsQuote(field) {
+			field = strconv.Quote(field)
+		}
+		fmt.Fprintf(ret, " %s=", colorize(field, lvlColor, !formatter.NoColor))
+
+		switch value := event[field].(type) {
+		case string:
+			if len(value) == 0 {
+				ret.WriteString("\"\"")
+			} else if needsQuote(value) {
+				ret.WriteString(strconv.Quote(value))
+			} else {
+				ret.WriteString(value)
+			}
+		case time.Time:
+			ret.WriteString(value.Format(time.RFC3339))
+		default:
+			b, err := jsoniter.Marshal(value)
+			if err != nil {
+				fmt.Fprintf(ret, "[error: %v]", err)
+			} else {
+				fmt.Fprint(ret, string(b))
+			}
+		}
+
+	}
+	ret.WriteByte('\n')
+
+	return ret.Bytes()
+}
