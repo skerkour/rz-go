a3f59b5bae3ac4764ef3e207e5bb05ab61287d99
z0mbie42 <sylvain@kerkour.com>
2019-02-07 22:40:54 +0100

    tests: update Ctx tests

--- ctx_test.go
+++ ctx_test.go
@@ -1,55 +1,57 @@
 package rz
 
-// func TestFromCtx(t *testing.T) {
-// 	log := New(Writer(ioutil.Discard))
-// 	ctx := log.ToCtx(context.Background())
-// 	log2 := FromCtx(ctx)
-// 	if !reflect.DeepEqual(log, *log2) {
-// 		fmt.Printf("log: %#v\n", log)
-// 		fmt.Printf("log: %#v\n", *log2)
-// 		t.Error("FromCtx did not return the expected logger")
-// 	}
+import (
+	"context"
+	"io/ioutil"
+	"testing"
+)
 
-// 	// update
-// 	log = log.Config(Level(InfoLevel))
-// 	ctx = log.ToCtx(ctx)
-// 	log2 = FromCtx(ctx)
-// 	if !reflect.DeepEqual(log, *log2) {
-// 		fmt.Printf("log: %#v\n", log)
-// 		fmt.Printf("log: %#v\n", *log2)
-// 		t.Error("FromCtx did not return the expected logger")
-// 	}
+func TestFromCtx(t *testing.T) {
+	log := New(Writer(ioutil.Discard))
+	ctx := log.ToCtx(context.Background())
+	log2 := FromCtx(ctx)
+	if &log != log2 {
+		t.Error("FromCtx did not return the expected logger")
+	}
 
-// 	log2 = FromCtx(context.Background())
-// 	if log2 != nil {
-// 		t.Error("FromCtx did not return the expected logger")
-// 	}
-// }
+	// update
+	log = log.Config(Level(InfoLevel))
+	ctx = log.ToCtx(ctx)
+	log2 = FromCtx(ctx)
+	if &log != log2 {
+		t.Error("FromCtx did not return the expected logger")
+	}
 
-// func TestFromCtxDisabled(t *testing.T) {
-// 	dl := New(Writer(ioutil.Discard), Level(Disabled))
-// 	ctx := dl.ToCtx(context.Background())
-// 	if ctx != context.Background() {
-// 		t.Error("ToCtx stored a disabled logger")
-// 	}
+	log2 = FromCtx(context.Background())
+	if log2 != nil {
+		t.Error("FromCtx did not return the expected logger")
+	}
+}
 
-// 	l := New(
-// 		Writer(ioutil.Discard),
-// 		With(func(e *Event) { e.String("foo", "bar") }),
-// 	)
-// 	ctx = l.ToCtx(ctx)
-// 	if FromCtx(ctx) != &l {
-// 		t.Error("WithContext did not store logger")
-// 	}
+func TestFromCtxDisabled(t *testing.T) {
+	dl := New(Writer(ioutil.Discard), Level(Disabled))
+	ctx := dl.ToCtx(context.Background())
+	if ctx != context.Background() {
+		t.Error("ToCtx stored a disabled logger")
+	}
 
-// 	l = l.Config(Level(DebugLevel))
-// 	ctx = l.ToCtx(ctx)
-// 	if FromCtx(ctx) != &l {
-// 		t.Error("ToCtx did not store copied logger")
-// 	}
+	l := New(
+		Writer(ioutil.Discard),
+		With(func(e *Event) { e.String("foo", "bar") }),
+	)
+	ctx = l.ToCtx(ctx)
+	if FromCtx(ctx) != &l {
+		t.Error("WithContext did not store logger")
+	}
 
-// 	ctx = dl.ToCtx(ctx)
-// 	if FromCtx(ctx) != &dl {
-// 		t.Error("ToCtx did not overide logger with a disabled logger")
-// 	}
-// }
+	l = l.Config(Level(DebugLevel))
+	ctx = l.ToCtx(ctx)
+	if FromCtx(ctx) != &l {
+		t.Error("ToCtx did not store copied logger")
+	}
+
+	ctx = dl.ToCtx(ctx)
+	if FromCtx(ctx) != &dl {
+		t.Error("ToCtx did not overide logger with a disabled logger")
+	}
+}
