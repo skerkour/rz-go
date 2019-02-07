a01ec044555451735bc299ec7ede163f24dcab38
z0mbie42 <sylvain@kerkour.com>
2019-02-07 13:40:01 +0100

    fix cicd

--- go.mod
+++ go.mod
@@ -1,3 +1,1 @@
 module github.com/bloom42/rz-go
-
-require github.com/pkg/errors v0.8.1
--- version.go
+++ version.go
@@ -2,5 +2,5 @@ package rz
 
 const (
 	// Version is the library's version
-	Version = "0.13.17"
+	Version = "0.13.18"
 )
