be9d0b6fffa49cd27021e00d35d0aa6b0caa5164
z0mbie42 <sylvain@kerkour.com>
2019-02-16 17:22:17 +0100

    fix: rz moduel import path

--- logger.go
+++ logger.go
@@ -8,7 +8,7 @@ import (
 	"sync"
 	"time"
 
-	"github.com/bloom42/rz-go/internal/json"
+	"github.com/bloom42/rz-go/v2/internal/json"
 )
 
 // A Logger represents an active logging object that generates lines
--- version.go
+++ version.go
@@ -2,5 +2,5 @@ package rz
 
 const (
 	// Version is the library's version
-	Version = "2.4.0"
+	Version = "2.4.1"
 )
